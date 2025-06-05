2c8362912cbc3d03ab57b3eced5defecb820d6f5
Durai-Prasanth <kdprasanth93@gmail.com>
2025-06-05 08:47:50 +0530

    Backend added port

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 
 // Middleware
 app.use(cors());
